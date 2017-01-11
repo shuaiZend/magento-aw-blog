6712dd004c9c9b03ed5ac5def7ef3a56b9427837
Ruan Azevedo <ruan.azevedo@gmail.com>
2017-01-11 04:05:57 -0200

    Add option to sort by year - must be refactored.

--- app/code/community/AW/Blog/Model/Post.php
+++ app/code/community/AW/Blog/Model/Post.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * aheadWorks Co.
  *
@@ -23,76 +24,91 @@
  * @copyright  Copyright (c) 2010-2012 aheadWorks Co. (http://www.aheadworks.com)
  * @license    http://ecommerce.aheadworks.com/AW-LICENSE.txt
  */
+class AW_Blog_Model_Post extends Mage_Core_Model_Abstract {
+	const NOROUTE_PAGE_ID = 'no-route';
 
+	protected function _construct() {
+		$this->_init('blog/post');
+	}
 
-class AW_Blog_Model_Post extends Mage_Core_Model_Abstract
-{
-    const NOROUTE_PAGE_ID = 'no-route';
+	public function load($id, $field = null) {
+		return $post = parent::load($id, $field);
+	}
 
-    protected function _construct()
-    {
-        $this->_init('blog/post');
-    }
+	public function noRoutePage() {
+		$this->setData($this->load(self::NOROUTE_PAGE_ID, $this->getIdFieldName()));
 
-    public function load($id, $field = null)
-    {
-        return $post = parent::load($id, $field);
-    }
+		return $this;
+	}
 
-    public function noRoutePage()
-    {
-        $this->setData($this->load(self::NOROUTE_PAGE_ID, $this->getIdFieldName()));
-        return $this;
-    }
+	public function getShortContent() {
+		$content = $this->getData('short_content');
+		if ( Mage::getStoreConfig(AW_Blog_Helper_Config::XML_BLOG_PARSE_CMS) ) {
+			$processor = Mage::getModel('core/email_template_filter');
+			$content   = $processor->filter($content);
+		}
 
-    public function getShortContent()
-    {
-        $content = $this->getData('short_content');
-        if (Mage::getStoreConfig(AW_Blog_Helper_Config::XML_BLOG_PARSE_CMS)) {
-            $processor = Mage::getModel('core/email_template_filter');
-            $content = $processor->filter($content);
-        }
-        return $content;
-    }
+		return $content;
+	}
 
-    public function getPostContent()
-    {
-        $content = $this->getData('post_content');
-        if (Mage::getStoreConfig(AW_Blog_Helper_Config::XML_BLOG_PARSE_CMS)) {
-            $processor = Mage::getModel('core/email_template_filter');
-            $content = $processor->filter($content);
-        }
-        return $content;
-    }
+	public function getPostContent() {
+		$content = $this->getData('post_content');
+		if ( Mage::getStoreConfig(AW_Blog_Helper_Config::XML_BLOG_PARSE_CMS) ) {
+			$processor = Mage::getModel('core/email_template_filter');
+			$content   = $processor->filter($content);
+		}
 
-    public function getCategoriesForPosts($posts = array())
-    {
-        return $this->getResource()->getCategoriesForPost($posts);
+		return $content;
+	}
 
-    }
+	public function getCategoriesForPosts($posts = array()) {
+		return $this->getResource()->getCategoriesForPost($posts);
 
-    public function loadByIdentifier($v)
-    {
-        return $this->load($v, 'identifier');
-    }
+	}
 
-    public function getCats()
-    {
-        $route = Mage::getStoreConfig('blog/blog/route');
-        if ($route == "") {
-            $route = "blog";
-        }
-        $route = Mage::getUrl($route);
+	public function loadByIdentifier($v) {
+		return $this->load($v, 'identifier');
+	}
 
-        $cats = Mage::getModel('blog/cat')->getCollection()
-            ->addPostFilter($this->getId())
-            ->addStoreFilter(Mage::app()->getStore()->getId())
-        ;
+	public function getCats() {
+		$route = Mage::getStoreConfig('blog/blog/route');
+		if ( $route == "" ) {
+			$route = "blog";
+		}
+		$route = Mage::getUrl($route);
 
-        $catUrls = array();
-        foreach ($cats as $cat) {
-            $catUrls[$cat->getTitle()] = $route . "cat/" . $cat->getIdentifier();
-        }
-        return $catUrls;
-    }
+		$cats = Mage::getModel('blog/cat')->getCollection()
+		            ->addPostFilter($this->getId())
+		            ->addStoreFilter(Mage::app()->getStore()->getId());
+
+		$catUrls = array();
+		foreach ( $cats as $cat ) {
+			$catUrls[ $cat->getTitle() ] = $route . "cat/" . $cat->getIdentifier();
+		}
+
+		return $catUrls;
+	}
+
+	public function getPostYears() {
+		$resource       = Mage::getSingleton('core/resource');
+		$readConnection = $resource->getConnection('core_read');
+		$query          = 'SELECT DISTINCT extract(YEAR FROM created_time) as year FROM aw_blog WHERE status=1';
+		$results        = $readConnection->fetchAll($query);
+		$years          = [];
+
+		foreach ( $results as $result ) {
+			$years[] = $result['year'];
+		}
+
+		return $years;
+	}
+
+	public function getPostsByYear($year) {
+		$posts = $this->getCollection()
+		              ->addFieldToFilter('created_time', [ 'from' => "{$year}-01-01" ])
+		              ->addFieldToFilter('created_time', [ 'to' => "{$year}-12-31" ])
+		              ->addFieldToFilter('status', 1);
+
+		return $posts;
+	}
 }
